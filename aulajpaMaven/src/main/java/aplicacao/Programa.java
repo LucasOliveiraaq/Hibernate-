90347d4b9aad6e34667c5372724560e159d7821c
Lucas Pinheiro de Oliveira <94260670+LucasOliveiraaq@users.noreply.github.com>
2022-12-04 12:03:56 -0300

    remove

--- aulajpaMaven/src/main/java/aplicacao/Programa.java
+++ aulajpaMaven/src/main/java/aplicacao/Programa.java
@@ -13,7 +13,9 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		Pessoa p = em.find(Pessoa.class, 2);
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 		System.out.println("Pronto");
 		em.close();
 		emf.close();
