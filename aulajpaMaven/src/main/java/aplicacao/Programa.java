003b4d96142bfbfd36dba11cb4b4439cf13062f8
Lucas Pinheiro de Oliveira <94260670+LucasOliveiraaq@users.noreply.github.com>
2022-12-04 11:57:38 -0300

    find encontra um objeto existente no banco

--- aulajpaMaven/src/main/java/aplicacao/Programa.java
+++ aulajpaMaven/src/main/java/aplicacao/Programa.java
@@ -9,19 +9,14 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Lucas teste", "lucas@gmail.com");
-		Pessoa p2 = new Pessoa(null, "sla teste", "sla@gmail.com");
-		Pessoa p3 = new Pessoa(null, "slasla teste", "slasla@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
 		System.out.println("Pronto");
+		em.close();
+		emf.close();
 	
 	}
 
